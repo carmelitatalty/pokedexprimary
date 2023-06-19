e9fd02f3cce2e0042fa46c3555b2643d7aa593f1
carmelitatalty <94077977+carmelitatalty@users.noreply.github.com>
2023-06-19 09:34:57 -0700

    added six pokemon to pokedex array

--- pokedex/js/scripts.js
+++ pokedex/js/scripts.js
@@ -2,5 +2,10 @@ alert('Hello world');
 let favoriteFood = 'coffee'
 document.write(favoriteFood);
 let pokemonlist = [
-    
+    {types: ['bug'], name: 'caterpie', height: '.3' },
+    {types: ['grass', 'poison'], name: 'bellsprout', height: '.7' }
+    {types: ['bug', 'flying'], name: 'beautifly', height: '1' }
+    {types: ['rock', 'ground'], name: 'onix', height: '8.8' }
+    {types: ['fighting'], name: 'machamp', height: '1.6' }
+    {types: ['electric'], name: 'Pikachu', height: '.4' }
 ]
