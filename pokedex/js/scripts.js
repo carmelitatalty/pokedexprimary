eaf496d23b3068282afe5ee228b6fb17730db51a
carmelitatalty <94077977+carmelitatalty@users.noreply.github.com>
2023-06-19 11:40:34 -0700

    added var for height, added for loop

--- pokedex/js/scripts.js
+++ pokedex/js/scripts.js
@@ -1,11 +1,19 @@
-alert('Hello world');
-let favoriteFood = 'coffee'
-document.write(favoriteFood);
 let pokemonlist = [
-    {types: ['bug'], name: 'caterpie', height: '.3' },
-    {types: ['grass', 'poison'], name: 'bellsprout', height: '.7' }
-    {types: ['bug', 'flying'], name: 'beautifly', height: '1' }
-    {types: ['rock', 'ground'], name: 'onix', height: '8.8' }
-    {types: ['fighting'], name: 'machamp', height: '1.6' }
-    {types: ['electric'], name: 'Pikachu', height: '.4' }
-]
+  { types: ["bug"], name: "caterpie", height: ".3" },
+  { types: ["grass", "poison"], name: "bellsprout", height: ".7" },
+  { types: ["bug", "flying"], name: "beautifly", height: "1" },
+  { types: ["rock", "ground"], name: "onix", height: "8.8" },
+  { types: ["fighting"], name: "machamp", height: "1.6" },
+  { types: ["electric"], name: "Pikachu", height: ".4" },
+];
+document.write("<ul>");
+for (let i = 0; i < pokemonlist.length; i++) {
+  let pokemon = pokemonlist[i];
+  let pokemontext = `${pokemon.name} (height: ${pokemon.height})`;
+
+  if (pokemon.height > 1) {
+    pokemontext = `${pokemontext} - WOW, that's big!`;
+  }
+  document.write(`<li>${pokemontext}</li>`);
+}
+document.write("</ul>");
