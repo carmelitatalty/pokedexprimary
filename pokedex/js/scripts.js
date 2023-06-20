935a87df938cc82dd6ed077b0d4b7ad38a83f753
carmelitatalty <94077977+carmelitatalty@users.noreply.github.com>
2023-06-19 19:27:03 -0700

    fixed merge conflicts

--- pokedex/js/scripts.js
+++ pokedex/js/scripts.js
@@ -7,23 +7,6 @@ let pokemonRepository = (function () {
     { types: ["fighting"], name: "machamp", height: "1.6" },
     { types: ["electric"], name: "Pikachu", height: ".4" },
   ];
-  function addListItem(pokemon) {
-    let listItem = document.createElement("li");
-    let ul = document.querySelector(".pokemon-list");
-    let button = document.createElement("button");
-    button.addEventListener('click', function (event) {
-    showDetails(pokemon)
-    });
-    button.classList.add("poke-button");
-    button.innerText = pokemon.name;
-    listItem.appendChild(button);
-    ul.appendChild(listItem);
-  }
-  function showDetails(pokemon) {
-    console.log(pokemon)
-  
-
-  }
   return {
     add: function (pokemon) {
       pokemonList.push(pokemon);
@@ -31,7 +14,55 @@ let pokemonRepository = (function () {
     getAll: function () {
       return pokemonList;
     },
-    addListItem: addListItem
   };
 })();
-pokemonRepository.getAll().forEach(pokemonRepository.addListItem)
+
+document.write("<ul>");
+pokemonRepository.getAll().forEach(function (pokemon) {
+  let pokemontext = `${pokemon.name} (height: ${pokemon.height})`;
+
+  if (pokemon.height > 1) {
+    pokemontext = `${pokemontext} - WOW, that's big!`;
+  }
+  document.write(`<li>${pokemontext}</li>`);
+});
+document.write("</ul>");
+
+//   console.log(pokemonRepository.getAll()); // []
+//   pokemonRepository.add({ name: 'Pikachu' });
+//   console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
+let pokemonRepository = (function () {
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+  // Other functions remain here
+
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    loadList: loadList
+  };
+})();
+
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
+});
